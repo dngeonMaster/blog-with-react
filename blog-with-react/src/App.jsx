6cc2c3c026de2dd8fcd6f5181243a806af0ed8f7
dngeonMasteer <kaushikborah4080@gmail.com>
2024-04-12 00:37:08 +0530

    redux config

--- blog-with-react/src/App.jsx
+++ blog-with-react/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout}  from './store/authSlice.js'
+import { Footer, Header } from './components/index.js'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
-  return (
-    <>
-      <h1>React</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
